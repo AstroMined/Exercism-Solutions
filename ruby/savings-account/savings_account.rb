7786c3165e1cdaa33dccd91ac68316a4dd25a6ac
Ryan Peterson <astromined@gmail.com>
2022-01-02 11:50:43 -0500

    Completed savings account exercise

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,48 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    @balance = balance
+    if @balance < 0
+      -3.213
+    elsif @balance < 1000
+      0.5
+    elsif @balance < 5000
+      1.621
+    else
+      2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    @balance = balance
+    if @balance < 0
+      @balance + (@balance * 0.03213)
+    elsif @balance < 1000
+      1.005 * @balance
+    elsif @balance < 5000
+      1.01621 * @balance
+    else
+      1.02475 * @balance
+    end  
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    @current_balance = current_balance
+    @desired_balance = desired_balance
+    @counter = 0
+    until @current_balance >= @desired_balance
+      @counter += 1
+      if @current_balance < 0
+        break
+      elsif @current_balance == 0
+        break
+      elsif @current_balance < 1000
+        @current_balance *= 1.005
+      elsif @current_balance < 5000
+        @current_balance *= 1.01621
+      else
+        @current_balance *= 1.02475
+      end
+    end
+    @counter
   end
 end
