d9bc832b77b43fc100b2427c5d9f1a399bede1c0
Ryan Peterson <astromined@gmail.com>
2022-01-02 17:13:43 -0500

    Completed Acronym exercise

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -5,3 +5,10 @@ Write your code for the 'Acronym' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
+
+class Acronym
+    def Acronym.abbreviate(phrase)
+        @phrase = phrase
+        @phrase.split(/ |-/).map { |str| str[0] }.join.upcase
+    end
+end
