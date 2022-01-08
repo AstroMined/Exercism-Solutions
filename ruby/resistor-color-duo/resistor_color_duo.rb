3e7e8632d552d4f0113f0b4314550ee55e004f38
Ryan Peterson <astromined@gmail.com>
2022-01-08 11:52:55 -0500

    Completed Resistory Color Duo exercise

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -5,3 +5,22 @@ Write your code for the 'Resistor Color Duo' exercise in this file. Make the tes
 To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
+
+class ResistorColorDuo
+    def ResistorColorDuo.value(colors)
+        @colors = colors
+        @color_one = @colors[0].capitalize().to_sym
+        @color_two = @colors[1].capitalize().to_sym
+        @color_to_int = {Black: "0",
+        Brown: "1",
+        Red: "2",
+        Orange: "3",
+        Yellow: "4",
+        Green: "5",
+        Blue: "6",
+        Violet: "7",
+        Grey: "8",
+        White: "9"}
+        (@color_to_int[@color_one] + @color_to_int[@color_two]).to_i
+    end
+end
