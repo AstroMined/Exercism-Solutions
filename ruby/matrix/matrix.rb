6428b87d1064690227ea157dba3f5551f68f8792
Ryan Peterson <astromined@gmail.com>
2022-01-08 22:35:28 -0500

    Completed Matrix exercise

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -5,3 +5,32 @@ Write your code for the 'Matrix' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
+class Matrix
+    def initialize(input)
+        @input = input
+    end
+
+    def rows
+        @rows = @input.split("\n")
+        @rows.each_with_index do |value,index|
+            @values = value.split(' ')
+            @values.each_with_index do |inner_value,inner_index|
+                @values[inner_index] = inner_value.to_i
+            end
+            @rows[index] = @values
+        end
+        @rows
+    end
+    
+    def columns
+        @rows = @input.split("\n")
+        @rows.each_with_index do |value,index|
+            @values = value.split(' ')
+            @values.each_with_index do |inner_value,inner_index|
+                @values[inner_index] = inner_value.to_i
+            end
+            @rows[index] = @values
+        end
+        @columns = @rows.transpose
+    end
+end
