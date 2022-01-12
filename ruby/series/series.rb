85e2dd4091ef433b87aa5d254cafb59e7f207334
Ryan Peterson <astromined@gmail.com>
2022-01-12 17:47:05 -0500

    Completed Series exercise

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -5,3 +5,22 @@ Write your code for the 'Series' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/series` directory.
 =end
+class Series
+    def initialize(input)
+        @input = input
+    end
+
+    def slices(slice_size)
+        @slice_size = slice_size
+
+        if @slice_size <= @input.split("").to_a.length()
+            @slices = @input.split("").each_cons(@slice_size).to_a
+            @slices.each_with_index do |slice,index|
+                @slices[index] = slice.join('')
+            end
+            @slices
+        else
+            raise ArgumentError
+        end
+    end
+end
