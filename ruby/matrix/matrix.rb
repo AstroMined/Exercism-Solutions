38635474d8e0b21287e416d9e589d1416b2e0d4c
Ryan Peterson <astromined@gmail.com>
2022-01-12 22:51:34 -0500

    Refactored Matrix exercise

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,36 +1,20 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
 class Matrix
-    def initialize(input)
-        @input = input
-    end
+  attr_reader :rows
 
-    def rows
-        @rows = @input.split("\n")
-        @rows.each_with_index do |value,index|
-            @values = value.split(' ')
-            @values.each_with_index do |inner_value,inner_index|
-                @values[inner_index] = inner_value.to_i
-            end
-            @rows[index] = @values
-        end
-        @rows
+  def initialize(input)
+    @input = input
+    @rows = @input.split("\n")
+    @rows.each_with_index do |value, index|
+      @values = value.split(' ')
+      @values.each_with_index do |inner_value, inner_index|
+        @values[inner_index] = inner_value.to_i
+      end
+      @rows[index] = @values
     end
-    
-    def columns
-        @rows = @input.split("\n")
-        @rows.each_with_index do |value,index|
-            @values = value.split(' ')
-            @values.each_with_index do |inner_value,inner_index|
-                @values[inner_index] = inner_value.to_i
-            end
-            @rows[index] = @values
-        end
-        @columns = @rows.transpose
-    end
+  end
+
+  def columns
+    @columns = @rows.transpose
+  end
 end
